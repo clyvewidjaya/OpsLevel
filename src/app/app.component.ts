2bcc684cc8402b2e4e7837b00f8e0813880863ac
Clyve Widjaya <clyvewidjaya@Clyves-MacBook-Pro.local>
2020-08-27 12:48:58 -0400

    without styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Todo} from './domain/Todo';
+import {FormControl, FormGroup} from '@angular/forms';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODOApp';
+  public todoList: Todo[] = [];
+  public onAddTodo = false;
+  public form: FormGroup;
+  public titleControl: FormControl;
+  public descriptionControl: FormControl;
+  public currentId = 0;
+
+  constructor() {
+    this.titleControl = new FormControl('');
+    this.descriptionControl = new FormControl('');
+    this.form = new FormGroup({
+      titleControl: this.titleControl,
+      descriptionControl: this.descriptionControl
+    });
+  }
+
+  public onDelete(id){
+    _.remove(this.todoList, (todo) => {
+      return todo.id === id;
+    });
+  }
+
+  public onAdd(){
+    this.onAddTodo = true;
+  }
+
+  public onCancel(){
+    this.onAddTodo = false;
+  }
+
+  public onSave(value){
+    this.todoList.push(
+        new Todo(this.currentId, value.titleControl, value.descriptionControl)
+    );
+
+    this.onAddTodo = false;
+    this.currentId++;
+    this.form.reset();
+  }
 }
---
+++ src/app/domain/Todo.ts
@@ -0,0 +1,11 @@
+export class Todo {
+    id: string;
+    title: string;
+    description: string;
+
+    constructor(id, title, description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+}
